979f224554a7cec1ce999d2721e6003d7f2e8e58
Starglen <starglen17@gmail.com>
2025-03-11 16:12:23 +0300

    fifth commit

--- HospitalSystem/settings.py
+++ HospitalSystem/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'HospitalSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'final-master',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
@@ -118,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR,'static'
 ]
 
 # Default primary key field type
